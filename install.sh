56e003399a8a8ef5663fc0656fd76ad26c690e0e
joeweaver <joe.e.weaver@gmail.com>
2021-03-19 18:58:58 +0000

    Support local machine files for i3

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ln -s ~/.my-dotfiles/.vimrc ~/.vimrc
 ln -s ~/.my-dotfiles/.bashrc ~/.bashrc
-ln -s ~/.my-dotfiles/.config/i3 ~/.config/
+#ln -s ~/.my-dotfiles/.config/i3 ~/.config/
+~/.my-dotfiles/.config/i3/combine_reload.sh
 ln -s ~/.my-dotfiles/.Xresources ~/.Xresources
 ln -s ~/.my-dotfiles/.cheat ~/.cheat
 ln -s ~/.my-dotfiles/.i3status.conf ~/.i3status.conf
